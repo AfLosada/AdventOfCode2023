c54600ba2985c6527d58ae5ad66734e00b9900ee
Andrés Felipe Losada luna <nedrocoli@gmail.com>
2023-12-08 22:50:56 -0500

    close to finish part 2 of day5 wtf this was unexpected

--- Day5/main.go
+++ Day5/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"slices"
+	"strconv"
 	"strings"
 )
 
@@ -14,33 +17,104 @@ func main() {
 
 	scanner := bufio.NewScanner(input)
 	scanner.Scan()
-	seeds := strings.Fields(strings.Split(scanner.Text(), ":")[1])
-	mapOfMaps := map[string]map[string]string{
-		"seed2Soil":            make(map[string]string),
-		"soil2fertilizer":      make(map[string]string),
-		"fertilizer2water":     make(map[string]string),
-		"water2light":          make(map[string]string),
-		"light2temperature":    make(map[string]string),
-		"temperature2humidity": make(map[string]string),
-		"humidity2location":    make(map[string]string),
+	seedRanges := strings.Fields(strings.Split(scanner.Text(), ":")[1])
+	seeds := windowed(seedRanges, 2)
+	mapOfMaps := map[string][]Range{
+		"seed2soil":            nil,
+		"soil2fertilizer":      nil,
+		"fertilizer2water":     nil,
+		"water2light":          nil,
+		"light2temperature":    nil,
+		"temperature2humidity": nil,
+		"humidity2location":    nil,
 	}
+	mapOrder := []string{}
 	var currentMap string
-
 	for scanner.Scan() {
 		line := scanner.Text()
-		if strings.Contains(line, ":") {
-			currentMap = strings.Replace(strings.Split(line, " ")[0], "-to-", "2", 1)
+		if line == "" {
 			continue
 		}
-		mapOfMaps[currentMap] = readLine(line)
+		if strings.Contains(line, ":") {
+			currentMap = strings.Replace(strings.Split(line, " ")[0], "-to-", "2", 1)
+			mapOrder = append(mapOrder, currentMap)
+			continue
+		}
+		mapOfMaps[currentMap] = append(mapOfMaps[currentMap], parseLineToMap(line))
+	}
+	locationList := []int{}
 
+	for _, seed := range seeds {
+		location := seed
+		for _, m := range mapOrder {
+			location = mapFromRanges(location, mapOfMaps[m])
+		}
+		locationList = append(locationList, location)
+	}
+	lowestLocation := slices.Min(locationList)
+	fmt.Printf("The lowest location number is: %d\n", lowestLocation)
+}
+
+type Range struct {
+	destinationStart int
+	sourceStart      int
+	length           int
+}
+
+func parseLineToMap(line string) Range {
+	splitLine := strings.Fields(line)
+	destinationStart, _ := strconv.Atoi(splitLine[0])
+	sourceStart, _ := strconv.Atoi(splitLine[1])
+	length, _ := strconv.Atoi(splitLine[2])
+	return Range{
+		destinationStart: destinationStart,
+		sourceStart:      sourceStart,
+		length:           length,
 	}
 }
 
-type Ranges struct {
-	destination []
+func mapFromRanges(number int, ranges []Range) int {
+	result := number
+	for _, r := range ranges {
+		result = mapFromRange(result, r)
+		if result != number {
+			return result
+		}
+	}
+	return result
 }
 
-func readLine(line string) map[string]string {
+func mapFromRange(number int, r Range) int {
+	inRange := (number >= r.sourceStart && number <= (r.sourceStart+r.length))
+	if !inRange {
+		return number
+	}
+	positionInRange := r.sourceStart - number
+	return r.destinationStart + -positionInRange
+}
 
+func windowed(slice []string, size int) [][]string {
+	var result [][]string
+
+	for i := 0; i <= len(slice)-size; i += size {
+		result = append(result, slice[i:i+size])
+	}
+
+	return result
+}
+
+func buildSeedsFromPairSlice(sliceOfPairs [][]string) []int {
+	result := []int{}
+	for _, pair := range sliceOfPairs {
+		start, _ := strconv.Atoi(pair[0])
+		amount, _ := strconv.Atoi(pair[1])
+		for i := 0; i < amount; i++ {
+			result = append(result, start+i)
+		}
+	}
+	return result
+}
+
+func findMinOfIntersectionOfRanges(seedRange Range, mapRange Range){
+	
 }
